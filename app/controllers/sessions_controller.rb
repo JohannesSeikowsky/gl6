a394d4f991b7a4ec1997ebee1bc0cad4c802fa5f
Johannes <seikowsky@googlemail.com>
2017-12-04 17:04:26 +0000

    ..

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,23 +1,26 @@
 class SessionsController < ApplicationController
   def create
-  	# create a user
-  	api_values = request.env['omniauth.auth']
-  	@user = User.new
-  	@user.uid = api_values['uid']
-	@user.provider = api_values['provider']
-	@user.name = api_values['info']['name']
-	@user.first_name = api_values['info']['first_name']
-	@user.last_name = api_values['info']['last_name']
-	@user.email = api_values['info']['email']
-	@user.location = api_values['info']['location']['name']
-	@user.country_code = api_values['info']['location']['country']['code']
-	@user.description = api_values['info']['description']
-	@user.image_url = api_values['info']['image']
-	@user.profile_url = api_values['info']['urls']['public_profile']
-
-	if @user.save
+  	begin
+	  	api_values = request.env['omniauth.auth']
+		# find or create
+		@user = find_or_create_by(uid: api_values['uid'])
+		# set attributes
+		@user.provider = api_values['provider']
+		@user.name = api_values['info']['name']
+		@user.first_name = api_values['info']['first_name']
+		@user.last_name = api_values['info']['last_name']
+		@user.email = api_values['info']['email']
+		@user.location = api_values['info']['location']['name']
+		@user.country_code = api_values['info']['location']['country']['code']
+		@user.description = api_values['info']['description']
+		@user.image_url = api_values['info']['image']
+		@user.profile_url = api_values['info']['urls']['public_profile']
+		@user.save!
+		# on success - login and redirect
+		session[:user_id] = @user.id	
 		redirect_to root_path, notice: "worked."	
-  	else
+  	rescue
+  		# on failure - redirect
   		redirect_to root_path, notice: "didnt work."
   	end
   end
