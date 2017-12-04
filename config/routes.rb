9e6a3e4487301e474c9ba5beff8ebc6490042657
Johannes <seikowsky@googlemail.com>
2017-12-04 17:44:42 +0000

    ..

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,5 @@ Rails.application.routes.draw do
 
   get '/auth/:provider/callback', to: 'sessions#create', as: 'create_session'
 
-  delete '/logout', to: 'sessions#destroy'
+  delete '/logout', to: 'sessions#destroy', as: 'logout'
 end
