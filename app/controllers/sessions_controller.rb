89b382948c92d9d59565ff0bf670f9b878cc7c0a
Johannes <seikowsky@googlemail.com>
2017-12-04 15:56:41 +0000

    ..

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,16 +3,16 @@ class SessionsController < ApplicationController
   	# create a user
   	api_values = request.env['omniauth.auth']
   	@user = {}
-  	@user.uid = api_values['uid']
-	@user.provider = api_values['provider']
-	@user.name = api_values['info']['name']
-	@user.first_name = api_values['info']['first_name']
-	@user.last_name = api_values['info']['last_name']
-	@user.email = api_values['info']['email']
-	@user.location = api_values['info']['location']['name']
-	@user.country_code = api_values['info']['location']['country']['code']
-	@user.description = api_values['info']['description']
-	@user.image_url = api_values['info']['image']
-	@user.profile_url = api_values['info']['urls']['public_profile']
+  	@user["uid"] = api_values['uid']
+	@user["provider"] = api_values['provider']
+	@user["name"] = api_values['info']['name']
+	@user["first_name"] = api_values['info']['first_name']
+	@user["last_name"] = api_values['info']['last_name']
+	@user["email"] = api_values['info']['email']
+	@user["location"] = api_values['info']['location']['name']
+	@user["country_code"] = api_values['info']['location']['country']['code']
+	@user["description"] = api_values['info']['description']
+	@user["image_url"] = api_values['info']['image']
+	@user["profile_url"] = api_values['info']['urls']['public_profile']
   end
 end
