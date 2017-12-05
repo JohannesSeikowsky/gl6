b91f0a3abb07637c0a6d876896cb8a9c2a734f86
Johannes <seikowsky@googlemail.com>
2017-12-05 16:28:56 +0000

    ..

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,6 @@ Rails.application.routes.draw do
 
   get 'user_account/:id', to: 'users#account', as: 'user_account'
 
-  post 'create_contact', to: 'contacts#new', 'create_contact'
+  post 'create_contact', to: 'contacts#new', as: 'create_contact'
 
 end
