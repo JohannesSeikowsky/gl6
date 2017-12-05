bce5de6d95d6f503cee729bed5b32bf86c77e149
Johannes <seikowsky@googlemail.com>
2017-12-05 16:41:30 +0000

    ..

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,6 +1,6 @@
 class ContactsController < ApplicationController
   def new
-  	@user = User.find(contacts_params[:contact][:user_id])
+  	@user = User.find(params[:contact][:user_id])
   	redirect_to user_account_path(@user)
   end
 
