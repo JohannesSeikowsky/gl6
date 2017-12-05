d5989ae68dd53a546cf279060436c66ae8715761
Johannes <seikowsky@googlemail.com>
2017-12-05 18:39:13 +0000

    ..

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,10 +1,15 @@
 class ContactsController < ApplicationController
   def new
   	@user = User.find(params[:contact][:user_id])
-  	@user.contacts.build(contacts_params)
-  	redirect_to user_account_path(@user)
+  	@contact = @user.contacts.build(contacts_params)
+  	if @contact.save
+  		redirect_to user_account_path(@user), notice: "contact created."
+ 	else
+  		redirect_to user_account_path(@user), notice: "try again."
+  	end	
   end
 
+  # params function
   def contacts_params
   	params.require(:contact).permit(:user_id, :name)
   end
