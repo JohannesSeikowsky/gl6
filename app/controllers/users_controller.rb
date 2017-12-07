dc4fe245f99a421e1abe13169c53cea1084ec5a7
Johannes <seikowsky@googlemail.com>
2017-12-07 17:09:19 +0000

    ..

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,6 @@
 class UsersController < ApplicationController
   def account
-  	if current_user
-  		@user = current_user
+  	if @user = current_user
 		@contacts = @user.contacts
   	else
   		redirect_to root_path, notice: "please log in."
