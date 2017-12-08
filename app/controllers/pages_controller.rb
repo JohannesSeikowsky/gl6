d3fc59a6266e96f81e4811d52f810697bc0ed270
Johannes <seikowsky@googlemail.com>
2017-12-08 21:17:17 +0000

    ..

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,7 +2,8 @@ class PagesController < ApplicationController
   def home
   	if current_user
   		redirect_to user_account_path(current_user)
+  	else
+	    render layout: false
   	end
-    render layout: false
   end
 end
