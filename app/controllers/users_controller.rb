84c402d94cd2b3ae9d0774b6427ae91a8094daec
Johannes <seikowsky@googlemail.com>
2017-12-12 15:33:25 +0000

    ...

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,11 @@
 class UsersController < ApplicationController
   def account
   	if @user = current_user
-		@contacts = @user.contacts
+		@contacts = @user.contacts.order('updated_at DESC')
+
   	else
   		redirect_to root_path, notice: "please log in."
 	end
   end
+    # @gees_contacts_all = @gees_contacts_all.sort_by { |a| a.this_month ? 0 : 1 }
 end
