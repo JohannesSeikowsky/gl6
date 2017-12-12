e1044b8bfd6e6aa05fff632fde7c85ab0eb53199
Johannes <seikowsky@googlemail.com>
2017-12-12 22:28:08 +0000

    ...

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,12 +1,21 @@
 class ProfilesController < ApplicationController
   def create_or_update_profile
-	
-	if contact.profile.nil?
-	  contact.create_profile(profile_params)
-	else
-	  contact.profile.update_attributes(profile_params)
-	end
-	
+    if current_user
+    	contact = Contact.find(params[:contact_id])
+    	if contact.profile.nil?
+	    	contact.build_profile(profile_params)
+	    	if contact.save
+	    		redirect_to user_account_path(current_user), notice: "Comment has been set."
+	    	else
+	    		redirect_to user_account_path(current_user), notice: "an error occured, try again."
+	    	end
+	    else
+    		contact.profile.update(profile_params)
+	    	redirect_to user_account_path(current_user), notice: "Comment has been updated."
+    	end
+    else
+      redirect_to root_path, notice: 'please log in.'
+    end
   end
 
   def profile_params
