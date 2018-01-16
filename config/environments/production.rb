68392130d0b310acf46047a1d89e4a31de7f0a19
Johannes <seikowsky@googlemail.com>
2018-01-16 21:12:43 +0000

    ...

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,18 @@
 Rails.application.configure do
+
+  # Setting up mailer with Gmail
+  config.action_mailer.delivery_method = :smtp
+  # SMTP settings for gmail
+  config.action_mailer.smtp_settings = {
+   :address              => "smtp.gmail.com",
+   :port                 => 587,
+   :domain               => "mindyournetwork.com",
+   :user_name            => ENV['gmail_username'],
+   :password             => ENV['gmail_password'],
+   :authentication       => "plain",
+  :enable_starttls_auto => true
+  }
+
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
