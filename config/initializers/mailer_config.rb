81aa2a897a7e756aaf156b610759ed84e839f48c
Johannes <seikowsky@googlemail.com>
2018-01-19 16:50:12 +0000

    ...

---
+++ config/initializers/mailer_config.rb
@@ -0,0 +1,12 @@
+ActionMailer::Base.smtp_settings = {
+  :address              => "smtp.gmail.com",
+  :port                 => 587,
+  :domain               => "mindyournetwork.com",
+   :user_name            => ENV['gmail_username'],
+   :password             => ENV['gmail_password'],
+  :authentication       => "plain",
+  :enable_starttls_auto => true
+}
+
+ActionMailer::Base.default_url_options[:host] = "mindyournetwork.com" if Rails.env.production?
+ActionMailer::Base.default_url_options[:host] = "http://localhost:3000/" if Rails.env.development?
