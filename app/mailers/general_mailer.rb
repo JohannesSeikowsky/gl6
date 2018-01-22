1ef3b2c8374e906e088f1b1bdf82cd533f0d855a
Johannes <seikowsky@googlemail.com>
2018-01-22 18:16:31 +0000

    ...

--- app/mailers/general_mailer.rb
+++ app/mailers/general_mailer.rb
@@ -4,6 +4,6 @@ class GeneralMailer < ApplicationMailer
   def signup_mail(users_first_name, users_email)
     @first_name = users_first_name
     @email = users_email
-    mail(to: @email, subject: 'test')
+    mail(to: @email, subject: 'Thank you!')
   end
 end
