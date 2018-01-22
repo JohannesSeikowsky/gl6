42a12ef270ce449642ef60630ce1271c904186a8
Johannes <seikowsky@googlemail.com>
2018-01-22 15:13:37 +0000

    ...

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,36 +1,34 @@
 class SessionsController < ApplicationController
   def create
   	begin
+  		# find or create
 	  	api_values = request.env['omniauth.auth']
-		# @user = User.find_or_create_by(uid: api_values['uid'])
-		if @user = User.find_by_uid(api_values['uid'])
-			# found a user.
-		else
-			# create a user.
-			@user = User.new
-			@user.uid = api_values['uid']
-			@user.provider = api_values['provider']
-			@user.name = api_values['info']['name']
-			@user.first_name = api_values['info']['first_name']
-			@user.last_name = api_values['info']['last_name']
-			@user.email = api_values['info']['email']
-			@user.location = api_values['info']['location']['name']
-			@user.country_code = api_values['info']['location']['country']['code']
-			@user.description = api_values['info']['description']
-			@user.image_url = api_values['info']['image']
-			@user.profile_url = api_values['info']['urls']['public_profile']
-			@user.save!
-			# GeneralMailer.signup_mail().deliver
-		end
+		@user = User.find_or_create_by(uid: api_values['uid'])
 
-		session[:user_id] = @user.id	
-		redirect_to user_account_path(id: current_user.id), notice: "youre logged in.")
+		@user.provider = api_values['provider']
+		@user.name = api_values['info']['name']
+		@user.first_name = api_values['info']['first_name']
+		@user.last_name = api_values['info']['last_name']
+		@user.email = api_values['info']['email']
+		@user.location = api_values['info']['location']['name']
+		@user.country_code = api_values['info']['location']['country']['code']
+		@user.description = api_values['info']['description']
+		@user.image_url = api_values['info']['image']
+		@user.profile_url = api_values['info']['urls']['public_profile']
+		@user.save!
 		
+		session[:user_id] = @user.id	
+		if current_user
+			redirect_to user_account_path(id: current_user.id), notice: "youre logged in."	
+		else
+			redirect_to root_path, notice: "didnt work, try again."
+		end
   	rescue
   		# on failure - redirect
   		redirect_to root_path, notice: "didnt work. try again."
   	end
   end
+
 
   def delete
     if current_user
