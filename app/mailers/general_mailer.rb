c7e31fbff993bc71458fced49b01b69695f0d38c
Johannes <seikowsky@googlemail.com>
2018-01-22 17:55:58 +0000

    ...

--- app/mailers/general_mailer.rb
+++ app/mailers/general_mailer.rb
@@ -1,9 +1,9 @@
 class GeneralMailer < ApplicationMailer
   default from: "mindyournetwork@gmail.com"
 
-  def signup_mail(user_first_name, user_email)
-    @first_name = user_first_name
-    @email = user_email
-    mail(to: "joseikowsky@gmail.com", subject: 'test')
+  def signup_mail(users_first_name, users_email)
+    @first_name = users_first_name
+    @email = users_email
+    mail(to: @email, subject: 'test')
   end
 end
