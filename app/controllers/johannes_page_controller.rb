67ada751613fcd4df5fe7d5854289fe329267ce0
Johannes <seikowsky@googlemail.com>
2018-03-01 16:20:30 +0000

    ...

--- app/controllers/johannes_page_controller.rb
+++ app/controllers/johannes_page_controller.rb
@@ -4,7 +4,7 @@ def email_subscriber
 	# create
 	Subscriber.create(email: params[:email_subscriber][:email])
 	# alert_mail
-	GeneralMailer.new_email_subscriber(params[:email_subscriber][:email])
+	GeneralMailer.new_email_subscriber(params[:email_subscriber][:email]).deliver
 	redirect_to root_path, notice: "It worked. Thank you!"
 end
 
@@ -14,7 +14,7 @@ def new_page_user
 		email: params[:new_page_user][:email])
 	# alert_mail
 	GeneralMailer.new_page_user(params[:new_page_user][:medium_link],
-		 params[:new_page_user][:email)
+		 params[:new_page_user][:email).deliver
 	redirect_to root_path, notice: "It worked. Thank you!"
 end
 
