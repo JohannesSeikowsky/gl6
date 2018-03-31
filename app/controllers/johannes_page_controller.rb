9615e0e2bbf5c7dfaece0d6b407d3ce0d73e84fc
Johannes <seikowsky@googlemail.com>
2018-03-31 14:01:56 +0100

    ...

--- app/controllers/johannes_page_controller.rb
+++ app/controllers/johannes_page_controller.rb
@@ -8,13 +8,13 @@ def email_subscriber
 	GeneralMailer.new_email_subscriber(@submitted_email, @author_of_page).deliver
 	
 	if @author_of_page == "valerie_vaughn"
-		redirect_to valerie_vaughn_path, notice: "It worked. Thank you!"
+		redirect_to valerie_vaughn_path, notice: "It worked! Thank you."
 	elsif @author_of_page == "dave_garbutt"
-		redirect_to dave_garbutt_path, notice: "It worked. Thank you!"	
+		redirect_to dave_garbutt_path, notice: "It worked! Thank you."	
 	elsif @author_of_page == "james_finn"
-		redirect_to james_finn_path, notice: "It worked. Thank you!"
+		redirect_to james_finn_path, notice: "It worked! Thank you,"
 	else
-		redirect_to root_path, notice: "It worked. Thank you!"
+		redirect_to root_path, notice: "It worked! Thank you."
 	end
 end
 
